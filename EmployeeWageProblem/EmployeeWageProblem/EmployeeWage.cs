77977aa8c1dd3b2243dc98c3afa50c4e32d6f7d1
Manoj Kumar Manukonda <manojkumarmanukonda77@gmail.com>
2022-10-22 09:10:36 +0530

    [Manoj] Modify: EmployeeWage

---
+++ EmployeeWageProblem/EmployeeWageProblem/EmployeeWage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public class EmployeeWage
+    {
+        public void Attendance()
+        {
+            Random random = new Random();
+        int empCheck = random.Next(0, 4);
+        if(empCheck ==0)
+            Console.WriteLine("Employee is present");
+        else
+            Console.WriteLine("Employee is present");
+        }
+        
+    }
+}
--- EmployeeWageProblem/EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/EmployeeWageProblem/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Problem");
+            EmployeeWage employeeWage = new EmployeeWage();
+            employeeWage.Attendance();
+        }
+    }
+}

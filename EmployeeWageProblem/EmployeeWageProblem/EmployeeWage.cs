f706d2d2de1dc9ffde617c948d4618a6accb8d00
Manoj Kumar Manukonda <manojkumarmanukonda77@gmail.com>
2022-10-26 08:36:25 +0530

    [Manoj] Updated UC1

---
+++ EmployeeWageProblem/EmployeeWageProblem/EmployeeWage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public class EmployeeWage
+    {
+        const int WAGE_PER_HR = 20,FULL_DAY_HR =8,IS_PRESENT =0,IS_FULL_TIME=1;
+        Random random = new Random();
+        public void Attendance()
+        {
+           
+        int empCheck = random.Next(0, 4);
+        if(empCheck ==0)
+            Console.WriteLine("Employee is present");
+        else
+            Console.WriteLine("Employee is present");
+        }
+        public void EmpWage()
+        {
+            int dailyEmpWage = 0, empHrs =0;
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+                empHrs = FULL_DAY_HR;
+            else
+                empHrs = 0;
+            dailyEmpWage = WAGE_PER_HR * empHrs;
+            Console.WriteLine("Daily Emp Wage "+ dailyEmpWage);
+        }
+        
+    }
+}
--- EmployeeWageProblem/EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/EmployeeWageProblem/Program.cs
@@ -1,2 +1,20 @@
+
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+﻿
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Problem");
+            EmployeeWage employeeWage = new EmployeeWage();
+           
+            employeeWage.EmpWage();
+        }
+    }
+}
+
